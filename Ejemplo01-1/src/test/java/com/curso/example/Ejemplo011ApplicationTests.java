a21c928f78273c66e49eefcd6aa7d875125a9ab1
KevinMoros <kevinoliver.toro@viewnext.com>
2024-05-30 12:49:06 +0200

    Ejemplo del MVC 1.

---
+++ Ejemplo01-1/src/test/java/com/curso/example/Ejemplo011ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo011ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
